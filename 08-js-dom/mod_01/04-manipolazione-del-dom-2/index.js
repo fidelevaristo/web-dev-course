971627e04f3b6a7c2502e54c19a368b27a0c461d
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 12:55:57 +0200

    feat(08-js-dom): mod_01 ex 04 - manipolazione del dom 2

---
+++ 08-js-dom/mod_01/04-manipolazione-del-dom-2/index.js
@@ -0,0 +1,28 @@
+﻿// Seleziona gli input
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const ageInput = document.querySelector("#age");
+
+// Recupera i valori degli input
+const firstName = firstNameInput.value;
+const lastName = lastNameInput.value;
+const age = Number(ageInput.value);
+
+// Crea l'oggetto person
+const person = {
+  firstName: firstName,
+  lastName: lastName,
+  age: age,
+};
+
+// Stampa l'oggetto nel console
+console.log(person);
+
+// Seleziona il form
+const form = document.querySelector("form");
+
+// Aggiunge l'attributo data-person con il JSON dell'oggetto
+form.setAttribute("data-person", JSON.stringify(person));
+
+// Stampa il form nel console
+console.log(form);
