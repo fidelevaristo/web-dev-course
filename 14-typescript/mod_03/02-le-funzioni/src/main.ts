1fc568b94f48144b241046cb9ca9113f57a409e5
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 13:19:48 +0200

    feat(14-typescript): mod_03 ex 02 - le funzioni

---
+++ 14-typescript/mod_03/02-le-funzioni/src/main.ts
@@ -0,0 +1,33 @@
+// Doubles a population value
+function doubleThePopulation(value: number): number {
+  return value * 2;
+}
+
+doubleThePopulation(5);
+
+doubleThePopulation(6.12);
+
+doubleThePopulation(8_526);
+
+// Displays the languages spoken in a country
+function languagesSpoken(
+  country: string,
+  language1: string,
+  language2?: string,
+): void {
+  console.log(`The languages spoken in ${country} are:`);
+
+  console.log(language1);
+
+  if (language2 !== undefined) {
+    console.log(language2);
+  }
+}
+
+languagesSpoken("Colombia", "Spanish", "English");
+
+languagesSpoken("Greece", "Greek");
+
+languagesSpoken("New Zealand", "English", "Māori");
+
+export {};
