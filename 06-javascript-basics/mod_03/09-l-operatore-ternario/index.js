d7bf0eb48dc611ebea81c8cdb7c92a762eed6de9
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:12 +0200

    feat(06-javascript-basics): mod_03 ex 09 - l-operatore ternario

---
+++ 06-javascript-basics/mod_03/09-l-operatore-ternario/index.js
@@ -0,0 +1,8 @@
+const isSunnyDay = true;
+const isHomeworkCompleted = true;
+
+const decision = (isSunnyDay && isHomeworkCompleted)
+  ? "Jesse can go out to play"
+  : "Jesse stays at home";
+
+console.log(decision);
