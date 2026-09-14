8548fe0be58f3d61f4d37914f2950aafb3aa87a6
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 12:55:38 +0200

    feat(08-js-dom): mod_01 ex 02 - cercare elementi nel dom

---
+++ 08-js-dom/mod_01/02-cercare-elementi-nel-dom/index.js
@@ -0,0 +1,11 @@
+﻿// 1. Cerca tutti gli elementi con classe "paragrafo"
+const paragrafi = document.querySelectorAll(".paragrafo");
+console.log(paragrafi);
+
+// 2. Cerca l'elemento con id "elemento-3"
+const elementoTre = document.getElementById("elemento-3");
+console.log(elementoTre);
+
+// 3. Cerca tutti gli elementi h2 della pagina
+const titoli = document.querySelectorAll("h2");
+console.log(titoli);
