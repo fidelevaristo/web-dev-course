db01c299fe27917b2c8cf963b334762348259cad
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 12:56:09 +0200

    feat(08-js-dom): mod_01 ex 05 - gli eventi del dom

---
+++ 08-js-dom/mod_01/05-gli-eventi-del-dom/index.js
@@ -0,0 +1,10 @@
+﻿// Seleziona il bottone
+const btn = document.querySelector(".btn");
+
+// Seleziona il body
+const body = document.body;
+
+// Aggiunge un evento click al bottone per attivare/disattivare la dark mode
+btn.addEventListener("click", () => {
+  body.classList.toggle("theme");
+});
