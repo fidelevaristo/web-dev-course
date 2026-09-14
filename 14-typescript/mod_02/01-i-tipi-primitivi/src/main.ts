719876fddb8750c935ef31df043ed144196ff782
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 13:19:31 +0200

    feat(14-typescript): mod_02 ex 01 - i tipi primitivi

---
+++ 14-typescript/mod_02/01-i-tipi-primitivi/src/main.ts
@@ -0,0 +1,25 @@
+// Country name
+let country: string = "Italy";
+
+// Undefined value
+let year: undefined = undefined;
+
+// Average temperature in Celsius
+let averageTemperature: number = 23.6;
+
+// Indicates whether the country has been visited
+let visited: boolean = true;
+
+// Currency not specified yet
+let currency: null = null;
+
+// Total population
+let population: number = 47_450_795;
+
+// Indicates if it is summer
+let isSummer: boolean = false;
+
+// Spoken languages
+let languages: string = "Spanish, English";
+
+export {};
