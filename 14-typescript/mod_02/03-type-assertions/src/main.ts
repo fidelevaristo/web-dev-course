33505cbc1f21b74da83b42abfdd23bd37060b8e1
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 13:19:37 +0200

    feat(14-typescript): mod_02 ex 03 - type assertions

---
+++ 14-typescript/mod_02/03-type-assertions/src/main.ts
@@ -0,0 +1,11 @@
+function getPopulation(): any {
+  return 69_950_850;
+}
+
+// Population is a number
+const population = getPopulation() as number;
+
+// Double the population value
+console.log(population * 2);
+
+export {};
