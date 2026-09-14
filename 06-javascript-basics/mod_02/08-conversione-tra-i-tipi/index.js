776072061b91f0dffc328f456c73ace609dedf54
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:09 +0200

    feat(06-javascript-basics): mod_02 ex 08 - conversione tra i tipi

---
+++ 06-javascript-basics/mod_02/08-conversione-tra-i-tipi/index.js
@@ -0,0 +1,11 @@
+let value = true;
+console.log(value, typeof value);
+
+value = Number(value);
+console.log(value, typeof value);
+
+value = String(value);
+console.log(value, typeof value);
+
+value = Boolean(value);
+console.log(value, typeof value);
