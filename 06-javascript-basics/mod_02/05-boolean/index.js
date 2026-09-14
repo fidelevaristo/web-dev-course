88fbc6a90c05df1b27bf9d6649966164619e39f6
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:09 +0200

    feat(06-javascript-basics): mod_02 ex 05 - boolean

---
+++ 06-javascript-basics/mod_02/05-boolean/index.js
@@ -0,0 +1,11 @@
+const itIsRaining = false;
+const hasPassedExam = true;
+const isUserLoggedIn = true;
+const canDrive = false;
+const isSummer = true;
+
+console.log("itIsRaining:", itIsRaining);
+console.log("hasPassedExam:", hasPassedExam);
+console.log("isUserLoggedIn:", isUserLoggedIn);
+console.log("canDrive:", canDrive);
+console.log("isSummer:", isSummer);
