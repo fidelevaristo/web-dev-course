6e9ed76aff882283a4bbc5beef3d228212f49432
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 12:55:32 +0200

    feat(08-js-dom): mod_01 ex 01 - navigare il dom

---
+++ 08-js-dom/mod_01/01-navigare-il-dom/index.js
@@ -0,0 +1,18 @@
+﻿// Seleziona il <body>
+const body = document.body;
+
+// Seleziona la <ul>
+const ul = body.children[0];
+
+// 1. Stampa l'elemento padre della <ul>
+console.log(ul.parentElement);
+
+// 2. Seleziona e stampa il secondo elemento figlio della <ul>
+const secondLi = ul.children[1];
+console.log(secondLi);
+
+// 3. Stampa il fratello successivo del secondo <li>
+console.log(secondLi.nextElementSibling);
+
+// 4. Stampa il fratello precedente del secondo <li>
+console.log(secondLi.previousElementSibling);
