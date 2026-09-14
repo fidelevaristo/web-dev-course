9299c2ec69a601746382f969d8577bcaead046c1
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:38:56 +0200

    feat(07-javascript-advanced): mod_01 ex 08 - template strings

---
+++ 07-javascript-advanced/mod_01/08-template-strings/index.js
@@ -0,0 +1,5 @@
+const multiLineString = `Questa è la prima riga del testo.
+Questa è la seconda riga, inclusa direttamente tramite template string.
+E questa è la terza riga finale.`;
+
+console.log(multiLineString);
