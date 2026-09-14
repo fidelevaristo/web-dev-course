8416c0deb5aa7eccfd246bed64762181348f1673
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:09 +0200

    feat(06-javascript-basics): mod_02 ex 06 - conversione tra i tipi

---
+++ 06-javascript-basics/mod_02/06-conversione-tra-i-tipi/index.js
@@ -0,0 +1,11 @@
+let hello = 'Ciao'; // boolean
+let age = 18; // string
+let isGraduated = false; // number
+
+hello = Boolean(hello);
+age = String(age);
+isGraduated = Number(isGraduated);
+
+console.log(hello, typeof hello);
+console.log(age, typeof age);
+console.log(isGraduated, typeof isGraduated);
