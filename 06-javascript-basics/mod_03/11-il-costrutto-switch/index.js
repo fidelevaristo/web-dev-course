c3340b735f96f42b43a2fa5bd73aabfb28f01557
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:12 +0200

    feat(06-javascript-basics): mod_03 ex 11 - il costrutto switch

---
+++ 06-javascript-basics/mod_03/11-il-costrutto-switch/index.js
@@ -0,0 +1,12 @@
+const favoriteFood = "pizza";
+
+switch (favoriteFood) {
+  case "pizza":
+    console.log("You like PIZZA!");
+    break;
+  case "coconut":
+    console.log("You like COCONUT!");
+    break;
+  default:
+    console.log("You like something else!");
+}
