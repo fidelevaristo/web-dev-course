7f5560f9adaebb674aac8ccbdb53ce102ea461e7
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:38:59 +0200

    feat(07-javascript-advanced): mod_02 ex 03 - settimeout e setinterval

---
+++ 07-javascript-advanced/mod_02/03-settimeout-setinterval/index.js
@@ -0,0 +1,6 @@
+function mostraPromemoria() {
+  console.log("Promemoria: È ora di fare una pausa!");
+}
+
+console.log("Il promemoria è in fase di impostazione");
+setTimeout(mostraPromemoria, 5000);
