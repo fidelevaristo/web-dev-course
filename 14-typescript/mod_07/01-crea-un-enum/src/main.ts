d4eeb20130ebe2356ba9e7fbd346532698bd876f
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 13:20:27 +0200

    feat(14-typescript): mod_07 ex 01 - crea un enum

---
+++ 14-typescript/mod_07/01-crea-un-enum/src/main.ts
@@ -0,0 +1,46 @@
+enum Currency {
+  Euro,
+  Dollar,
+}
+
+interface Country {
+  name: string;
+  currency: Currency;
+}
+
+const countries: Country[] = [
+  {
+    name: "France",
+    currency: Currency.Euro,
+  },
+  {
+    name: "United States of America",
+    currency: Currency.Dollar,
+  },
+  {
+    name: "Italy",
+    currency: Currency.Euro,
+  },
+  {
+    name: "New Zealand",
+    currency: Currency.Dollar,
+  },
+];
+
+enum LanguageStatus {
+  Primary = "primary",
+  Secondary = "secondary",
+}
+
+const countryLanguages = [
+  {
+    language: "Spanish",
+    status: LanguageStatus.Primary,
+  },
+  {
+    language: "English",
+    status: LanguageStatus.Secondary,
+  },
+];
+
+export {};
