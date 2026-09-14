80eb2424d67ba3b05e71f64e01df8267f39e9a48
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:38:57 +0200

    feat(07-javascript-advanced): mod_01 ex 12 - la destrutturazione

---
+++ 07-javascript-advanced/mod_01/12-la-destrutturazione/index.js
@@ -0,0 +1,7 @@
+const colors = ["red", "green", "blue"];
+
+const [firstColor, secondColor, thirdColor] = colors;
+
+console.log("Primo colore:", firstColor);
+console.log("Secondo colore:", secondColor);
+console.log("Terzo colore:", thirdColor);
