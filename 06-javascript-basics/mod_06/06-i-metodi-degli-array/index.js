2b0fffa5f7bc5b9b5f1350ab4cfa1031e25b1d4c
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:17 +0200

    feat(06-javascript-basics): mod_06 ex 06 - i metodi degli array

---
+++ 06-javascript-basics/mod_06/06-i-metodi-degli-array/index.js
@@ -0,0 +1,10 @@
+const students = [
+  { id: 1, name: "Luca", surname: "Rossi", age: 20 },
+  { id: 2, name: "Mario", surname: "Bianchi", age: 24 },
+  { id: 3, name: "Giovanni", surname: "Brambilla", age: 30 },
+];
+
+// Rimuovere Giovanni (all'indice 2) e inserire Francesco
+students.splice(2, 1, { id: 3, name: "Francesco", surname: "Verdi", age: 41 });
+
+console.log(students);
