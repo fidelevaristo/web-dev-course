1510bc3b20b1a927a23108400aad48df9eb5392a
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:08 +0200

    feat(06-javascript-basics): mod_02 ex 02 - i nomi delle variabili

---
+++ 06-javascript-basics/mod_02/02-i-nomi-delle-variabili/index.js
@@ -0,0 +1,11 @@
+// Nomi di variabili corretti secondo le regole degli identificatori JavaScript:
+// - Non possono iniziare con un numero (es. 1age -> age)
+// - Non possono contenere caratteri speciali tranne _ e $ (es. *surname -> surname, ?isMarried -> isMarried, job? -> job)
+let name = 'mario';
+let surname = 'Rossi';
+let age = 20;
+let isMarried = false;
+let job = 'developer';
+let isGraduated2 = false;
+
+console.log({ name, surname, age, isMarried, job, isGraduated2 });
