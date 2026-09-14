bf0296b3583ab23bbbc2797533ac71e1e6881c1e
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:16 +0200

    feat(06-javascript-basics): mod_05 ex 07 - nested objects

---
+++ 06-javascript-basics/mod_05/07-nested-objects/index.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  school: "Liceo",
+  year: 3,
+  personalData: {
+    name: "Fidel",
+    surname: "Vicente Evaristo",
+    age: 20,
+  },
+};
+
+console.log(student.personalData);
