2c5e7b94bd459a83a170034a24ac79250ad7eb1c
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 14:29:08 +0200

    feat(06-javascript-basics): mod_02 ex 01 - i nomi delle variabili

---
+++ 06-javascript-basics/mod_02/01-i-nomi-delle-variabili/index.js
@@ -0,0 +1,22 @@
+// Rettangolo
+const rectangleBase = 10;
+const rectangleHeight = 5;
+
+// Cerchio
+const circleRadius = 7;
+
+// Prezzo cibi
+const pizzaPrice = 8.5;
+const focacciaPrice = 4.0;
+const applePrice = 1.2;
+
+console.log("Base rettangolo:", rectangleBase);
+console.log("Altezza rettangolo:", rectangleHeight);
+console.log("Raggio cerchio:", circleRadius);
+console.log("Prezzo pizza:", pizzaPrice);
+console.log("Prezzo focaccia:", focacciaPrice);
+console.log("Prezzo mela:", applePrice);
+
+// Per dare priorità alla somma utilizziamo le parentesi tonde ()
+const sumMultipliedByTwo = (5 + 3) * 2;
+console.log("Somma moltiplicata per due:", sumMultipliedByTwo);
