a9cc9e6b44d05862d4fb440331136ecc06a1cd71
Fidel Vicente Evaristo <fidelevaristo7@gmail.com>
2026-09-14 13:37:17 +0200

    feat(16-react): mod_04 ex 01 - form non controllati

---
+++ 16-react/mod_04/01-form-non-controllati/src/App.tsx
@@ -0,0 +1,11 @@
+import { UncontrolledLogin } from "./components/UncontrolledLogin";
+
+function App() {
+  return (
+    <div>
+      <UncontrolledLogin />
+    </div>
+  );
+}
+
+export default App;
